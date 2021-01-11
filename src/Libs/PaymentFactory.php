a7b0e20268853f012698b2aa0c69b985a6027d82
Guilherme Assis <guilherme.assis@codificar.com.br>
2021-01-11 11:48:21 -0300

    fix consts

--- src/Libs/PaymentFactory.php
+++ src/Libs/PaymentFactory.php
@@ -3,50 +3,41 @@
 namespace Codificar\PaymentGateways\Libs;
 
 use Settings;
+use Codificar\PaymentGateways\PaymentFacade;
 
 class PaymentFactory
 {
-    const PAYMENT_GATEWAY_PAGARME = 'pagarme';
-    const PAYMENT_GATEWAY_STRIPE = 'stripe';
-    const PAYMENT_GATEWAY_ZOOP = 'zoop';
-    const PAYMENT_GATEWAY_CIELO = 'cielo';
-    const PAYMENT_GATEWAY_BRASPAG = 'braspag';
-    const PAYMENT_GATEWAY_DIRECTPAY = 'directpay';
-    const PAYMENT_GATEWAY_GETNET = 'getnet';
-    const PAYMENT_GATEWAY_BANCARD = 'bancard';
-    const PAYMENT_GATEWAY_TRANSBANK = 'transbank';
-    const PAYMENT_GATEWAY_GERENCIANET = 'gerencianet';
 
     public static function createGateway()
     {
         switch (Settings::findByKey('default_payment')) {
-            case self::PAYMENT_GATEWAY_PAGARME:
+            case PaymentFacade::PAYMENT_GATEWAY_PAGARME:
                 return (new PagarmeLib());
 
-            case self::PAYMENT_GATEWAY_STRIPE:
+            case PaymentFacade::PAYMENT_GATEWAY_STRIPE:
                 return (new StripeLib());
 
-            case self::PAYMENT_GATEWAY_ZOOP:
+            case PaymentFacade::PAYMENT_GATEWAY_ZOOP:
                 return (new ZoopLib());
 
-            case self::PAYMENT_GATEWAY_CIELO:
+            case PaymentFacade::PAYMENT_GATEWAY_CIELO:
                 return (new CieloLib());
 
-            case self::PAYMENT_GATEWAY_BRASPAG:
+            case PaymentFacade::PAYMENT_GATEWAY_BRASPAG:
                 return (new BrasPagLib());
 
-            case self::PAYMENT_GATEWAY_GETNET:
+            case PaymentFacade::PAYMENT_GATEWAY_GETNET:
                 return (new GetNetLib());
 
-            case self::PAYMENT_GATEWAY_DIRECTPAY:
+            case PaymentFacade::PAYMENT_GATEWAY_DIRECTPAY:
                 return (new DirectPayLib());
 
-            case self::PAYMENT_GATEWAY_BANCARD:
+            case PaymentFacade::PAYMENT_GATEWAY_BANCARD:
                 return (new BancardLib());
-            case self::PAYMENT_GATEWAY_TRANSBANK:
+            case PaymentFacade::PAYMENT_GATEWAY_TRANSBANK:
                 return (new TransbankLib());
 
-            case self::PAYMENT_GATEWAY_GERENCIANET:
+            case PaymentFacade::PAYMENT_GATEWAY_GERENCIANET:
                 return (new GerenciaNetLib());
         }
     }
@@ -54,7 +45,7 @@ class PaymentFactory
     public static function createBilletGateway()
     {
         switch (Settings::findByKey('default_payment_boleto')) {
-            case self::PAYMENT_GATEWAY_GERENCIANET:
+            case PaymentFacade::PAYMENT_GATEWAY_GERENCIANET:
                 return (new GerenciaNetLib());
         }
     }
--- src/PaymentFacade.php
+++ src/PaymentFacade.php
@@ -7,6 +7,18 @@ use Codificar\PaymentGateways\Libs\PaymentFactory;
 
 class PaymentFacade extends \Illuminate\Support\Facades\Facade
 {
+
+    const PAYMENT_GATEWAY_PAGARME = 'pagarme';
+    const PAYMENT_GATEWAY_STRIPE = 'stripe';
+    const PAYMENT_GATEWAY_ZOOP = 'zoop';
+    const PAYMENT_GATEWAY_CIELO = 'cielo';
+    const PAYMENT_GATEWAY_BRASPAG = 'braspag';
+    const PAYMENT_GATEWAY_DIRECTPAY = 'directpay';
+    const PAYMENT_GATEWAY_GETNET = 'getnet';
+    const PAYMENT_GATEWAY_BANCARD = 'bancard';
+    const PAYMENT_GATEWAY_TRANSBANK = 'transbank';
+    const PAYMENT_GATEWAY_GERENCIANET = 'gerencianet';
+
     /**
      * @param  array  $parameters
      * @return createGateway
