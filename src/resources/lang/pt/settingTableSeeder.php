19fcd556e5260d710c1fd0cf7557b816f49dca19
Guilherme Assis <guilherme.assis@codificar.com.br>
2021-04-28 10:00:30 -0300

    pt portugal

---
+++ src/resources/lang/pt/settingTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+	'default_pay_gate'						=> 'Intermediador Padrão de Pagamento'
+	,'pay_encryption_key_me' 				=> 'Chave de Criptografia do Pagar.me'
+	,'pagarme_recipient_id' 				=> 'Id de Recebedor do Pagar.me'	
+	,'pagarme_api_key' 						=> 'Chave de API do Pagar.me'
+	,'stripe_secret' 						=> 'Chave Privada do Stripe'
+	,'stripe_public' 						=> 'Chave Pública do Stripe'
+	,'stripe_connect' 						=> 'Stripe Connect'
+
+	,'braspag_merchant_id'					=> 'braspag merchant id'
+	,'braspag_merchant_key'					=> 'braspag merchant key'
+	,'getnet_client_id'						=> 'getnet client id'
+	,'getnet_client_secret'					=> 'getnet client secret'
+	,'getnet_seller_id'						=> 'getnet seller id'
+	,'directpay_encrypt_key'				=> 'directpay encrypt key'
+	,'directpay_encrypt_value'				=> 'directpay encrypt value'
+	,'directpay_requester_id'				=> 'directpay requester id'
+	,'directpay_requester_password'			=> 'directpay requester password'
+	,'directpay_requester_token'			=> 'directpay requester token'
+	,'directpay_unique_trx_id'				=> 'directpay unique trx id'
+	,'cielo_merchant_id'					=> 'cielo merchant id'
+	,'cielo_merchant_key'					=> 'cielo merchant key'
+	,'zoop_marketplace_id'					=> 'zoop marketplace id'
+	,'zoop_publishable_key'					=> 'zoop publishable key'
+	,'zoop_seller_id'						=> 'zoop seller id'
+	,'bancard_public_key'					=> 'bancard public key'
+	,'bancard_private_key'					=> 'bancard private key'
+	,'transbank_private_key'				=> 'transbank private key'
+	,'transbank_commerce_code'				=> 'transbank commerce code'
+	,'transbank_public_cert'				=> 'transbank public cert'
+	,'prepaid_min_billet_value'				=> 'prepaid min billet value'
+	,'prepaid_tax_billet'					=> 'prepaid tax billet'
+
+);
