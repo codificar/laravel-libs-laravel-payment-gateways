2500117a14edc41b838c591b8453172dffdde290
oto.luz <oto.luz@codificar.com.br>
2021-07-15 19:42:16 -0300

    ipag db conf

---
+++ src/Database/migrations/2021_07_15_190813_ipag_conf.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class IpagConf extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $ipagId     =   Settings::findObjectByKey('ipag_api_id');
+        $ipagKey    =   Settings::findObjectByKey('ipag_api_key');
+        $ipagToken  =   Settings::findObjectByKey('ipag_token');
+
+        if(!$ipagId)
+            Settings::updateOrCreate(array('key' => 'ipag_api_id',
+                'value'         => '', 
+                'tool_tip'      => 'Ipag API Id', 
+                'page'          => '1',
+                'category'      => '6',
+                'sub_category'  => '0',
+            ));
+        
+        if(!$ipagKey)
+            Settings::updateOrCreate(array('key' => 'ipag_api_key',
+                'value'         => '', 
+                'tool_tip'      => 'Ipag API Key', 
+                'page'          => '1',
+                'category'      => '6',
+                'sub_category'  => '0',
+            ));    
+
+        if(!$ipagToken)
+            Settings::updateOrCreate(array('key' => 'ipag_token',
+                'value'         => '', 
+                'tool_tip'      => 'Ipag Token', 
+                'page'          => '1',
+                'category'      => '6',
+                'sub_category'  => '0',
+            ));    
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $ipagId     =   Settings::findObjectByKey('ipag_api_id');
+        $ipagKey    =   Settings::findObjectByKey('ipag_api_key');
+        $ipagToken  =   Settings::findObjectByKey('ipag_token');
+
+        if (!$ipagId) {
+            $ipagId->delete();
+        }
+        
+        if (!$ipagKey) {
+            $ipagKey->delete();
+        }
+
+        if (!$ipagToken) {
+            $ipagToken->delete();  
+        }
+    }
+}
